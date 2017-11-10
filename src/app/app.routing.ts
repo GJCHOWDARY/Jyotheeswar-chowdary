5c519dcbf2f7ac3576f2a32bf65f9e6117ea8cce
chowdary001 <jyotheeswar.chowary@habilitertechnologies.com>
2017-11-11 00:23:50 +0530

    first commit

---
+++ src/app/app.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './landing/landing.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: 'resume',             component: HomeComponent },
+    { path: 'about',     component: ProfileComponent },
+    { path: 'contact',           component: SignupComponent },
+    { path: 'home',          component: LandingComponent },
+    { path: 'nucleoicons',      component: NucleoiconsComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
